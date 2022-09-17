9dcb3bfbc6ce915723e9430e706a61faa019250c
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-09-17 14:21:58 -0700

    conditionally render pages from onClick in Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
+export default function Nav(props) {
+  //import props
+  const { pages = [], currentPage, setCurrentPage } = props;
 
-export default function Nav() {
-//see mod 20.1.6 if want to make links an array and map over
+  //conditional set title to current page
+  useEffect(() => {
+    document.title = currentPage.name;
+  }, [currentPage]);
 
-    return (
-      <header className="flex-row">
-        <h1>
-            <a className="href-links" href='/'>Robin Ostrowski</a>
-        </h1>
-        <nav>
-            <ul className="flex-row">
-                <li className="flex-row">
-                    <a className="href-links" href="#about">About Me</a>
-                </li>
-                <li>
-                    <a className="href-links" href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a className="href-links" href="#resume">Resume</a>
-                </li>
-                <li>
-                    <span className="href-links">Contact</span>
-                </li>
-            </ul>
-        </nav>
-      </header>
-    );
-  }
+  return (
+    //return header and navigation
+    <header className="flex-row">
+      <h1>
+        <a className="href-links" href="/">
+          Robin Ostrowski
+        </a>
+      </h1>
+      <nav>
+        <ul className="flex-row">
+          {/* //map over pages to display links */}
+          {pages.map((page) => (
+            <li
+              className={`href-links ${
+                currentPage.name === page.name && "navActive"
+              }`}
+              key={page.name}
+            >
+              {/* change currentPage when clicked */}
+              <span
+                onClick={() => {
+                  setCurrentPage(page);
+                }}
+              >
+                {page.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
